f2cd853e1a76333842111b2d482f3a41f562a1df
KaedeKR <xjhrz1826@gmail.com>
2025-05-20 14:29:54 +0800

    2025.5.20

--- test.cpp
+++ test.cpp
@@ -1,52 +1,11 @@
 ﻿#include <bits/stdc++.h>
+using namespace std;
 
-typedef struct Node {
-    int data;
-    struct Node* next;
-} Node;
-
-Node* newNode(int data);
-
-/*关键函数*/
-void reverseList(Node* head) {
-    Node* p = head->next;
-    Node* q;
-
-    head->next = NULL;
-
-    while (q != NULL) {
-        q = p->next;
-        p->next = head->next;
-        head->next = p;
-        p = q;
-    }
-}
-
-Node* newNode(int data) {
-    Node* node = (Node*)malloc(sizeof(Node));
-    node -> data = data;
-    node -> next = NULL;
-    return node;
+void printVersion() {
+    cout << __VERSION__ << endl;
 }
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    Node* L = newNode(-1);
-    Node* last = L;
-    for (int i = 0; i < n; i++) {
-        int data;
-        scanf("%d", &data);
-        last -> next = newNode(data);
-        last = last -> next;
-    }
-    reverseList(L);
-    Node* temp = L -> next;
-    printf("%d", temp -> data);
-    temp = temp -> next;
-    while (temp != NULL) {
-        printf(" %d", temp -> data);
-        temp = temp -> next;
-    }
+    printVersion();
     return 0;
 }
